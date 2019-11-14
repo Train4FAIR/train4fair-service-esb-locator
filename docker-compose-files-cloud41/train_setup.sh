875b849b557b2bbc99bd99d15c1f714aae766dae
Joao Bosco Jares MSc <jbjares@gmail.com>
2019-11-14 17:10:37 +0100

    - minor fix

--- docker-compose-files-cloud41/train_setup.sh
+++ docker-compose-files-cloud41/train_setup.sh
@@ -4,28 +4,34 @@ echo "Welcome to Train Platform Setup!"
 echo "We are installing the -=Train Platform=- locally"
 echo "Version 1.0.0 (NightBuild:301020190431) for evaluation purpose, only."
 
-
+echo "Loading..."
 if ([ $1 = "--init" ] && [ $2 = true ]) && ([ $3 = "--env" ] && [ $4 = "dev" ]); then
+echo "Mode [ --init:true & --env:dev ]"
 
-	rm -r /app/service-locator-data/
-	rm -r /app/service-locator-docker/
-	rm -r /app/common/docker-files/
-	rm -r /webdav/metadata/
-	rm -r /webdav/landingpage/
-	rm -r /webdav/documentation/
+
+	echo "Removing old folders..."
+	sudo rm -r /app/service-locator-data/
+	sudo rm -r /app/service-locator-docker/
+	sudo rm -r /app/common/docker-files/
+	sudo rm -r /webdav/metadata/
+	sudo rm -r /webdav/landingpage/
+	sudo rm -r /webdav/documentation/
 	
-	mkdir -p /app/service-locator-data/
-	mkdir -p /app/service-locator-docker/
-	mkdir -p /app/common/docker-files/
-	mkdir -p /webdav/metadata/
-	mkdir -p /webdav/landingpage/
-	mkdir -p /webdav/documentation/
+	echo "Creating new folders..."
+	sudo mkdir -p /app/service-locator-data/
+	sudo mkdir -p /app/service-locator-docker/
+	sudo mkdir -p /app/common/docker-files/
+	sudo mkdir -p /webdav/metadata/
+	sudo mkdir -p /webdav/landingpage/
+	sudo mkdir -p /webdav/documentation/
 	
-	cp wait-for-it.sh /app/common/docker-files/
-	cp mongo/sd_environmentproperties.json /app/common/docker-files/
-	cp sd_entrypoint.sh /app/common/docker-files/
-	cp esb_entrypoint.sh /app/common/docker-files/
+	echo "Copying new config files..."
+	sudo cp wait-for-it.sh /app/common/docker-files/wait-for-it.sh
+	sudo cp mongo/sd_environmentproperties.json /app/common/docker-files/sd_environmentproperties.json
+	sudo cp sd_entrypoint.sh /app/common/docker-files/sd_entrypoint.sh
+	sudo cp esb_entrypoint.sh /app/common/docker-files/esb_entrypoint.sh
 	
+	echo "Updating folder permissions..."
 	chmod -R 777 /app/service-locator-data/
 	chmod -R 777 /app/service-locator-docker/
 	chmod -R 777 /app/common/docker-files/
@@ -34,13 +40,21 @@ if ([ $1 = "--init" ] && [ $2 = true ]) && ([ $3 = "--env" ] && [ $4 = "dev" ]);
 	chmod -R 777 /webdav/documentation/
 	chmod -R 777 ./
 	
+	echo "Components Train SD and ESB are setting up..."
+	echo "docker-compose loading..."
+	sudo docker-compose up -d
+	echo "docker-compose finished."
+	echo "Components Train SD and ESB are set up!"	
+	
+		
+	echo "Deploying the client..."
+	echo "docker-compose loading..."
+	sudo docker-compose -t docker-compose-red.yml up -d
+	echo "docker-compose finished."
+	echo "Client are set up!"
 fi
 
-echo "Components Train SD and ESB are setting up..."
-echo "docker-compose loading..."
-sudo docker-compose up -d 
-echo "docker-compose finished."
-echo "Components Train SD and ESB are set up!"
+
 
 if ([ $5 = "--init-dav" ] && [ $6 = true ]); then
 	echo "Webdav servers are setting up on ports 9999 (The metadata Repository), 9998 (The Landing Pages Repository) and 9997 (The Documentation Repository)"
---
+++ src/test/java/de/fraunhofer/fit/train/facade/LoadDBCloudMenzelTest.java
@@ -0,0 +1,219 @@
+package de.fraunhofer.fit.train.facade;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.fraunhofer.fit.train.config.AppConfig;
+import de.fraunhofer.fit.train.model.EnvENUM;
+import de.fraunhofer.fit.train.model.EnvironmentProperties;
+import de.fraunhofer.fit.train.model.dev.DEVenvironmentProperties;
+import de.fraunhofer.fit.train.model.dev.MicroservicesPropertiesDEVModel;
+import de.fraunhofer.fit.train.model.dev.MongoMicroservicesPropertiesDEVModel;
+import de.fraunhofer.fit.train.model.dev.TrainModellingToolPropertiesDEVModel;
+import de.fraunhofer.fit.train.model.dev.TrainNodesPropertiesDEVModel;
+import de.fraunhofer.fit.train.model.dev.WebdavMicroServicesPropertiesDEVModel;
+import de.fraunhofer.fit.train.model.prod.MicroservicesPropertiesPRODModel;
+import de.fraunhofer.fit.train.model.prod.MongoMicroservicesPropertiesPRODModel;
+import de.fraunhofer.fit.train.model.prod.PRODenvironmentProperties;
+import de.fraunhofer.fit.train.model.prod.TrainModellingToolPropertiesPRODModel;
+import de.fraunhofer.fit.train.model.prod.TrainNodesPropertiesPRODModel;
+import de.fraunhofer.fit.train.model.prod.WebdavMicroServicesPropertiesPRODModel;
+import de.fraunhofer.fit.train.model.test.MicroservicesPropertiesTESTModel;
+import de.fraunhofer.fit.train.model.test.MongoMicroservicesPropertiesTESTModel;
+import de.fraunhofer.fit.train.model.test.TESTenvironmentProperties;
+import de.fraunhofer.fit.train.model.test.TrainModellingToolPropertiesTESTModel;
+import de.fraunhofer.fit.train.model.test.TrainNodesPropertiesTESTModel;
+import de.fraunhofer.fit.train.model.test.WebdavDocumentationPropertiesTESTModel;
+import de.fraunhofer.fit.train.model.test.WebdavMetadataPropertiesTESTModel;
+import de.fraunhofer.fit.train.model.test.WebdavMicroServicesPropertiesTESTModel;
+import de.fraunhofer.fit.train.model.test.WebdavPagePropertiesTESTModel;
+import de.fraunhofer.fit.train.persistence.IEnvironmentPropertiesRepository;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = {AppConfig.class})
+@ComponentScan({"de.fraunhofer.fit.train"})
+@EntityScan("de.fraunhofer.fit.train")
+@SpringBootApplication
+public class LoadDBCloudMenzelTest {
+	
+	
+	@Autowired IEnvironmentPropertiesRepository repository;
+	
+
+	
+	@Test
+	public void trainMockNotNull() {
+		Assert.assertNotNull(repository);
+	}
+	
+	
+	@Test
+	public void targetInsert() throws NoSuchAlgorithmException, IOException {
+		repository.deleteAll();
+		repository.save(getEnvMockProperties());
+	}
+	
+	public EnvironmentProperties getEnvMockProperties() {
+		
+		EnvironmentProperties evprops = new EnvironmentProperties();
+		DEVenvironmentProperties dev = new DEVenvironmentProperties();
+		dev.setEnv(EnvENUM.DEV.toString());
+		MicroservicesPropertiesDEVModel microservicesPropertiesDEVModel = new MicroservicesPropertiesDEVModel();
+		microservicesPropertiesDEVModel.setHost("0.0.0.0");
+		microservicesPropertiesDEVModel.setPort("9091");
+		microservicesPropertiesDEVModel.setUser("admin");
+		microservicesPropertiesDEVModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		dev.setMicroservicesPropertiesDEVModel(microservicesPropertiesDEVModel);
+				
+		MongoMicroservicesPropertiesDEVModel mongoMicroservicesPropertiesDEVModel = new MongoMicroservicesPropertiesDEVModel();
+		mongoMicroservicesPropertiesDEVModel.setHost("cloud41.dbis.rwth-aachen.de");
+		mongoMicroservicesPropertiesDEVModel.setPort("27017");
+		mongoMicroservicesPropertiesDEVModel.setUser("admin");
+		mongoMicroservicesPropertiesDEVModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		dev.setMongoMicroservicesPropertiesDEVModel(mongoMicroservicesPropertiesDEVModel);
+	
+		TrainModellingToolPropertiesDEVModel trainModellingToolPropertiesDEVModel = new TrainModellingToolPropertiesDEVModel();
+		trainModellingToolPropertiesDEVModel.setHost("127.0.0.1");
+		trainModellingToolPropertiesDEVModel.setPort("1880");
+		trainModellingToolPropertiesDEVModel.setUser("admin");
+		trainModellingToolPropertiesDEVModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		dev.setTrainModellingToolPropertiesDEVModel(trainModellingToolPropertiesDEVModel);
+		
+		TrainNodesPropertiesDEVModel trainNodesPropertiesDEVModel = new TrainNodesPropertiesDEVModel();
+		trainNodesPropertiesDEVModel.setHost("127.0.0.1");
+		trainNodesPropertiesDEVModel.setPort("1880");
+		trainNodesPropertiesDEVModel.setUser("admin");
+		trainNodesPropertiesDEVModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		dev.setTrainNodesPropertiesDEVModel(trainNodesPropertiesDEVModel);
+		
+		WebdavMicroServicesPropertiesDEVModel webdavMicroServicesPropertiesDEVModel = new WebdavMicroServicesPropertiesDEVModel();
+		webdavMicroServicesPropertiesDEVModel.setHost("127.0.0.1");
+		webdavMicroServicesPropertiesDEVModel.setPort("9999");
+		webdavMicroServicesPropertiesDEVModel.setUser("admin");
+		webdavMicroServicesPropertiesDEVModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		dev.setWebdavMicroServicesPropertiesDEVModel(webdavMicroServicesPropertiesDEVModel);
+		evprops.setDEVenvironmentProperties(dev);
+		
+		//==PROD
+		PRODenvironmentProperties prod = new PRODenvironmentProperties();
+		prod.setEnv("PROD");
+		MicroservicesPropertiesPRODModel microservicesPropertiesPRODModel = new MicroservicesPropertiesPRODModel();
+		microservicesPropertiesPRODModel.setHost("127.0.0.1");
+		microservicesPropertiesPRODModel.setPort("9091");
+		microservicesPropertiesPRODModel.setUser("admin");
+		microservicesPropertiesPRODModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		prod.setMicroservicesPropertiesPRODModel(microservicesPropertiesPRODModel);
+				
+		MongoMicroservicesPropertiesPRODModel mongoMicroservicesPropertiesPRODModel = new MongoMicroservicesPropertiesPRODModel();
+		mongoMicroservicesPropertiesPRODModel.setHost("127.0.0.1");
+		mongoMicroservicesPropertiesPRODModel.setPort("27017");
+		mongoMicroservicesPropertiesPRODModel.setUser("admin");
+		mongoMicroservicesPropertiesPRODModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		prod.setMongoMicroservicesPropertiesPRODModel(mongoMicroservicesPropertiesPRODModel);
+	
+		TrainModellingToolPropertiesPRODModel trainModellingToolPropertiesPRODModel = new TrainModellingToolPropertiesPRODModel();
+		trainModellingToolPropertiesPRODModel.setHost("127.0.0.1");
+		trainModellingToolPropertiesPRODModel.setPort("1880");
+		trainModellingToolPropertiesPRODModel.setUser("admin");
+		trainModellingToolPropertiesPRODModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		prod.setTrainModellingToolPropertiesPRODModel(trainModellingToolPropertiesPRODModel);
+		
+		
+		TrainNodesPropertiesPRODModel trainNodesPropertiesPRODModel = new TrainNodesPropertiesPRODModel();
+		trainNodesPropertiesPRODModel.setHost("127.0.0.1");
+		trainNodesPropertiesPRODModel.setPort("1880");
+		trainNodesPropertiesPRODModel.setUser("admin");
+		trainNodesPropertiesPRODModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		prod.setTrainNodesPropertiesPRODModel(trainNodesPropertiesPRODModel);
+		
+		WebdavMicroServicesPropertiesPRODModel webdavMicroServicesPropertiesPRODModel = new WebdavMicroServicesPropertiesPRODModel();
+		webdavMicroServicesPropertiesPRODModel.setHost("127.0.0.1");
+		webdavMicroServicesPropertiesPRODModel.setPort("9999");
+		webdavMicroServicesPropertiesPRODModel.setUser("admin");
+		webdavMicroServicesPropertiesPRODModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		prod.setWebdavMicroServicesPropertiesPRODModel(webdavMicroServicesPropertiesPRODModel);
+		evprops.setPRODenvironmentProperties(prod);
+		
+		//==TEST
+		TESTenvironmentProperties test = new TESTenvironmentProperties();
+		test.setEnv("TEST");
+		MicroservicesPropertiesTESTModel microservicesPropertiesTESTModel = new MicroservicesPropertiesTESTModel();
+		microservicesPropertiesTESTModel.setHost("menzel.informatik.rwth-aachen.de");
+		microservicesPropertiesTESTModel.setPort("9091");
+		microservicesPropertiesTESTModel.setUser("admin");
+		microservicesPropertiesTESTModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		test.setMicroservicesPropertiesTESTModel(microservicesPropertiesTESTModel);
+				
+		MongoMicroservicesPropertiesTESTModel mongoMicroservicesPropertiesTESTModel = new MongoMicroservicesPropertiesTESTModel();
+		mongoMicroservicesPropertiesTESTModel.setHost("menzel.informatik.rwth-aachen.de");
+		mongoMicroservicesPropertiesTESTModel.setPort("27017");
+		mongoMicroservicesPropertiesTESTModel.setUser("admin");
+		mongoMicroservicesPropertiesTESTModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		test.setMongoMicroservicesPropertiesTESTModel(mongoMicroservicesPropertiesTESTModel);
+	
+		TrainModellingToolPropertiesTESTModel trainModellingToolPropertiesTESTModel = new TrainModellingToolPropertiesTESTModel();
+		trainModellingToolPropertiesTESTModel.setHost("train.platform.de");
+		trainModellingToolPropertiesTESTModel.setPort("1880");
+		trainModellingToolPropertiesTESTModel.setUser("admin");
+		trainModellingToolPropertiesTESTModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		test.setTrainModellingToolPropertiesTESTModel(trainModellingToolPropertiesTESTModel);
+		
+		
+		TrainNodesPropertiesTESTModel trainNodesPropertiesTESTModel = new TrainNodesPropertiesTESTModel();
+		trainNodesPropertiesTESTModel.setHost("train.platform.de");
+		trainNodesPropertiesTESTModel.setPort("1880");
+		trainNodesPropertiesTESTModel.setUser("admin");
+		trainNodesPropertiesTESTModel.setPass("21232f297a57a5a743894a0e4a801fc3");
+		test.setTrainNodesPropertiesTESTModel(trainNodesPropertiesTESTModel);
+		
+		WebdavMicroServicesPropertiesTESTModel webdavMicroServicesPropertiesTESTModel = new WebdavMicroServicesPropertiesTESTModel();
+		webdavMicroServicesPropertiesTESTModel.setHost("167.172.175.112");
+		webdavMicroServicesPropertiesTESTModel.setPort("9999");
+		webdavMicroServicesPropertiesTESTModel.setUser("gofair");
+		webdavMicroServicesPropertiesTESTModel.setPass("gofair");
+		test.setWebdavMicroServicesPropertiesTESTModel(webdavMicroServicesPropertiesTESTModel);
+		
+		WebdavMetadataPropertiesTESTModel webdavMetadataPropertiesTESTModel = new WebdavMetadataPropertiesTESTModel();
+		webdavMetadataPropertiesTESTModel.setHost("167.172.175.112");
+		webdavMetadataPropertiesTESTModel.setPort("9999");
+		webdavMetadataPropertiesTESTModel.setUser("gofair");
+		webdavMetadataPropertiesTESTModel.setPass("gofair");
+		test.setWebdavMetadataPropertiesTESTModel(webdavMetadataPropertiesTESTModel);
+		
+		WebdavDocumentationPropertiesTESTModel webdavDocumentationPropertiesTESTModel = new WebdavDocumentationPropertiesTESTModel();
+		webdavDocumentationPropertiesTESTModel.setHost("167.172.175.112");
+		webdavDocumentationPropertiesTESTModel.setPort("9997");
+		webdavDocumentationPropertiesTESTModel.setUser("gofair");
+		webdavDocumentationPropertiesTESTModel.setPass("gofair");
+		test.setWebdavDocumentationPropertiesTESTModel(webdavDocumentationPropertiesTESTModel);
+		
+		WebdavPagePropertiesTESTModel webdavPagePropertiesTESTModel = new WebdavPagePropertiesTESTModel();
+		webdavPagePropertiesTESTModel.setHost("167.172.175.112");
+		webdavPagePropertiesTESTModel.setPort("9998");
+		webdavPagePropertiesTESTModel.setUser("gofair");
+		webdavPagePropertiesTESTModel.setPass("gofair");
+		test.setWebdavPagePropertiesTESTModel(webdavPagePropertiesTESTModel);
+		//test.setWebdavMicroServicesPropertiesTESTModel(webdavMicroServicesPropertiesTESTModel);
+		
+		evprops.setTESTenvironmentProperties(test);
+
+		return evprops;
+		
+	}
+
+	
+}
